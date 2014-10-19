dcc97d8bccdc677f8d4cccb2e98dd8f4c6e50b46
Jean-Philippe Ouellet <jpo@vt.edu>
2014-10-19 14:48:55 -0400

    basic (and kinda bad) spammer simulation support

--- index/benchmark.sh
+++ index/benchmark.sh
@@ -2,13 +2,14 @@
 
 trace() {
 	printf "[%s]\n" "$1"
-	./tracegen.sh $2 $3 $4 $5 > traces/$1
+	./tracegen.sh $2 $3 $4 $5 $6 $7 $8 > traces/$1
 	make bench TRACE_FILE=traces/$1 | sort -rn
 	echo
 }
 
 mkdir -p traces
 
-trace typical 100 50 10 5
-trace dos 1000 5 1 3
-trace high-volume 10 20 10 40
+trace typical 100 50 10 5 1000 30 2
+trace dos 1000 5 1 3 1000 0 0
+trace high-volume 10 20 10 40 1000 0 0
+trace spammers 100 50 10 5 500 40 1
--- index/tracegen.sh
+++ index/tracegen.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-if [ $# -ne 4 ]; then
+if [ $# -ne 7 ]; then
 	echo "Usage: $0 n_ppl max_msgs msg_int ppl_int" 1>&2
 	printf "\tnumber of unique people\n" 1>&2
 	printf "\tmax number of messages per person\n" 1>&2
 	printf "\tmax message interval per person\n" 1>&2
 	printf "\tmax new person interval\n" 1>&2
+	printf "\tspam chance\n" 1>&2
+	printf "\tmessages per spamming run\n" 1>&2
+	printf "\tspam message interval\n" 1>&2
 	exit 1
 fi
 
@@ -13,6 +16,9 @@ n_ppl=$1;	shift
 max_msgs=$1;	shift
 msg_int=$1;	shift
 ppl_int=$1;	shift
+spam_chance=$1;	shift
+spam_msgs=$1;	shift
+spam_int=$1;	shift
 
 name_max=12
 name_min=6 # long enough that dupes are improbable
@@ -27,8 +33,21 @@ pick_name() {
 	| head_c $((($RANDOM % ($name_max - $name_min)) + $name_min))
 }
 
+now=
+
 update_time() {
-	echo $(($1 + ($RANDOM % $msg_int)))
+	now=$(($now + ($RANDOM % $1)))
+}
+
+spam_check() {
+	if [ $(($RANDOM % $spam_chance)) -eq 0 ]; then
+		i=0
+		while [ $i -lt $spam_msgs ]; do
+			i=$(($i+1))
+			printf "%s\t %s\n" $now $1
+			update_time $spam_int
+		done
+	fi
 }
 
 generate() {
@@ -37,21 +56,23 @@ generate() {
 	while [ $i -lt $n_ppl ]; do
 		i=$(($i+1))
 
-		base=$(($RANDOM % $ppl_int + $last_base))
-		last_base=$base
+		now=$(($RANDOM % $ppl_int + $last_base))
+		last_base=$now
 
 		name=$(pick_name)
 
-		printf "%s\t+%s\n" $base $name
-		now=$(update_time $base)
+		printf "%s\t+%s\n" $now $name
+		update_time $msg_int
 
 		n_msgs=$(($RANDOM % $max_msgs))
 		j=0
 		while [ $j -lt $n_msgs ]; do
 			j=$(($j+1))
 
+			spam_check $name
+
 			printf "%s\t %s\n" $now $name
-			now=$(update_time $now)
+			update_time $msg_int
 		done
 
 		printf "%s\t-%s\n" $now $name
