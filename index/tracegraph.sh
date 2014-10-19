c074122e5481405bb6bd83b76a8f8e5f64e7b2f3
Jean-Philippe Ouellet <jpo@vt.edu>
2014-10-19 04:45:31 -0400

    filter bars by user-specified verbosity level
    level 0 - bars changing in a different direction than previously
    level 1 - bars different than the previous one
    level 2 - all bars

--- index/tracegraph.sh
+++ index/tracegraph.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+[ -n "$1" ] && v=$1 || v=0
+
 bar() {
 	if [ $1 -gt 0 ]; then
 		head -c $1 /dev/zero | tr -C x x
@@ -7,11 +9,33 @@ bar() {
 	echo
 }
 
+last_dir=
+dir=
+
+dir_change() {
+	[ -n "$last_dir" -a -n "$dir" -a X"$last_dir" != X"$dir" ]
+}
+
 x=0
+max=0
 while read; do
 	case $REPLY in
-	+*) x=$(($x + 1));;
-	-*) x=$(($x - 1));;
+	+*)
+		dir=up
+		x=$(($x + 1))
+		[ $x -gt $max ] && max=$x
+		( dir_change || [ $v -ge 1 ] ) && bar $x
+		;;
+	-*)
+		dir=down
+		x=$(($x - 1))
+		( dir_change || [ $v -ge 1 ] ) && bar $x
+		;;
+	*)
+		[ $v -ge 2 ] && bar $x
+		;;
 	esac
-	bar $x
+	last_dir=$dir
 done
+
+echo max: $max
