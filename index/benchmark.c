7cd5bc54ad3c9971759ed02707a79b5c9bec5d62
Jean-Philippe Ouellet <jpo@vt.edu>
2014-10-19 00:06:04 -0400

    perform actual benchmarking

--- index/benchmark.c
+++ index/benchmark.c
@@ -1,3 +1,6 @@
+#include <sys/time.h>
+#include <sys/resource.h>
+
 #include <assert.h>
 #include <err.h>
 #include <errno.h>
@@ -9,7 +12,7 @@
 
 #include "index.h"
 
-#define N_ROUNDS (100)
+#define DEFAULT_SECS (3)
 
 bool quiet = false;
 
@@ -108,23 +111,69 @@ do_round(struct index *idx)
 	return 0;
 }
 
+volatile sig_atomic_t stop;
+
+void
+do_stop(int sig)
+{
+	stop = 1;
+}
+
+#define USEC(x) ((double)(x) / (double)1e6)
+
+void
+stats(const struct rusage *begin, const struct rusage *end, size_t rounds)
+{
+	time_t usr_s_delta, sys_s_delta, s_delta;
+	suseconds_t usr_us_delta, sys_us_delta, us_delta;
+	double usr_delta, sys_delta, delta, usr_rate, rate;
+
+	usr_s_delta = end->ru_utime.tv_sec - begin->ru_utime.tv_sec;
+	sys_s_delta = end->ru_stime.tv_sec - begin->ru_stime.tv_sec;
+	s_delta = usr_s_delta + sys_s_delta;
+
+	usr_us_delta = end->ru_utime.tv_usec - begin->ru_utime.tv_usec;
+	sys_us_delta = end->ru_stime.tv_usec - begin->ru_stime.tv_usec;
+	us_delta = usr_us_delta + sys_us_delta;
+
+	usr_delta = (double)usr_s_delta + USEC(usr_us_delta);
+	sys_delta = (double)sys_s_delta + USEC(sys_us_delta);
+	delta = (double)s_delta + USEC(us_delta);
+
+	usr_rate = (double)rounds / usr_delta;
+	rate = (double)rounds / delta;
+
+	printf("%ld\t%lf\t%lf\t%lf\t%lf\t%lf\t%s\n",
+	    rounds, usr_delta, sys_delta, delta, usr_rate, rate,
+	    getprogname());
+}
+
 void
 usage(void)
 {
-	fprintf(stderr, "Usage: %s [-q] index.so ...\n", getprogname());
+	fprintf(stderr, "Usage: %s [-q] [-t secs] index.so ...\n", getprogname());
 	exit(1);
 }
 
 int
 main(int argc, char *argv[])
 {
+	struct rusage begin, end;
+	struct itimerval timer;
 	struct index *idx;
-	int ch, i, ret;
+	int ch, ret;
+	time_t secs = DEFAULT_SECS;
+	size_t round;
 
-	while ((ch = getopt(argc, argv, "q")) != -1) {
+	while ((ch = getopt(argc, argv, "qt:")) != -1) {
 		switch (ch) {
 		case 'q':
 			quiet = true;
+			break;
+		case 't':
+			secs = atoi(optarg);
+			if (secs <= 0)
+				usage();
 			break;
 		default:
 			usage();
@@ -134,19 +183,30 @@ main(int argc, char *argv[])
 	argc -= optind;
 	argv += optind;
 
-	/* TODO init timer */
-	for (i = 0; i < N_ROUNDS; i++) {
+	timer.it_interval.tv_sec = 0;
+	timer.it_interval.tv_usec = 0;
+	timer.it_value.tv_sec = secs;
+	timer.it_value.tv_usec = 0;
+
+	signal(SIGPROF, do_stop);
+
+	round = 0;
+	stop = 0;
+	setitimer(ITIMER_PROF, &timer, NULL);
+	getrusage(RUSAGE_SELF, &begin);
+	do {
 		idx = index_new();
 		if (idx == NULL)
-			errx(1, "index_new failed in round %d", i);
+			errx(1, "index_new failed in round %zu", round);
 
-		/* TODO start timer */
 		ret = do_round(idx);
-		/* TODO stop timer */
 		if (ret != 0)
-			errx(1, "failed in round %d", i);
-	}
-	/* TODO print timer */
+			errx(1, "failed in round %zu", round);
+		round++;
+	} while (!stop);
+	getrusage(RUSAGE_SELF, &end);
+
+	stats(&begin, &end, round);
 
 	return 0;
 }
